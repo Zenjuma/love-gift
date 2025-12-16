14430b31557ae828ff29aa430a74e29a0d13ffc5
Zenjuma <jumaalizen8@gmail.com>
2025-12-16 19:11:23 +0300

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import "../css/Home.css"
+
+export default function Home(){
+
+    return(
+        <div className="container text-center home align-content-center ">
+
+           
+           <h2 className="fs-2">Hi baby</h2>
+           <h1 className="fs-1">Happy Christmas</h1>
+           <p>Do you want to see your little gift?</p>
+
+           <button type="button" class="btn btn-light btn-sm me-4">Small button</button>
+           <button type="button" class="btn btn-light btn-sm ms-4">Small button</button>
+           
+
+        </div>
+       
+    )
+}
