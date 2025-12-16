ee5c337e4c52a91e0da0c86b7f7d970f72d3b711
Zenjuma <jumaalizen8@gmail.com>
2025-12-16 20:02:01 +0300

    hi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,8 @@ export default function Home(){
            <h1 className="fs-1 title">Happy Christmas</h1>
            <p className="normal">Do you want to see your little gift?</p>
 
-           <button type="button" class="btn btn-light btn-md me-4">Yes Please</button>
-           <button type="button" class="btn btn-light btn-md ms-4">No Thanks</button>
+           <button type="button" class="btn btn-light btn-md me-4 rounded-4">Yes Please</button>
+           <button type="button" class="btn btn-light btn-md ms-4 rounded-4">No Thanks</button>
            
 
         </div>
