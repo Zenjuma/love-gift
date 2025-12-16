a0b1c6ccfcfa88b334b7fa66b117d79b0a1ec777
Nick Dieda <nickeagle888@gmail.com>
2025-12-16 19:51:23 +0300

    love

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,9 @@ export default function Home(){
         <div className="container text-center home align-content-center ">
 
            
-           <h2 className="fs-2">Hi baby</h2>
-           <h1 className="fs-1">Happy Christmas</h1>
-           <p>Do you want to see your little gift?</p>
+           <h2 className="fs-2 val">Hi Baby</h2>
+           <h1 className="fs-1 title">Happy Christmas</h1>
+           <p className="text">Do you want to see your little gift?</p>
 
            <button type="button" class="btn btn-light btn-sm me-4">Small button</button>
            <button type="button" class="btn btn-light btn-sm ms-4">Small button</button>
