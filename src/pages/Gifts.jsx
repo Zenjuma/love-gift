2e1ad4514152592bbfe060628bf623c3fb052278
Zenjuma <jumaalizen8@gmail.com>
2025-12-16 21:19:31 +0300

    hi

--- src/pages/Gifts.jsx
+++ src/pages/Gifts.jsx
@@ -1,9 +1,40 @@
 import "../css/Gifts.css"
 
+
 export default function Gifts(){
     return (
-        <div className="container gifts text-center home align-content-center ">
-            <h1>THESE ARE FOR YOU</h1>
+        <div className="container gifts text-center home align-content-center" >
+            <h1 className="title">THESE ARE FOR YOU</h1>
+            <p>Click to open</p>
+
+    <div className="mb-3  d-flex justify-content-evenly">
+
+    <div className="card me-2  " >
+  
+  <div className="card-body">
+    <h5>Light card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+  </div>
+
+  <div className="card me-2" >
+
+   <div className="card-body">
+    <h5>Light card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+  </div>
+
+  <div className="card " >
+
+   <div className="card-body">
+    <h5>Light card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+  </div>
+</div>
+</div>
+
+
         </div>
     )
 }
