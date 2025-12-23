b86d0df66271e9e5e86a33c0caf50e2717c16f71
Nick Dieda <nickeagle888@gmail.com>
2025-12-23 19:57:52 +0300

    theta

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,13 @@
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import "../css/Button.css"
 
 export default function Button(){
-    const navigate = useNavigate();
 
 
     return(
-        <div className="btn btn-light m-5"
-        onClick={() => navigate(-1)}>
-            <i className="bi bi-caret-left back-btn text-danger "></i>
-            Back
-
-        </div>
+        <Link to="/" className="position-fixed btn btn-light text-danger fw-bolder">
+        <i className="bi bi-arrow-left-short me-2"></i>
+        Back
+      </Link>
     )
 }
